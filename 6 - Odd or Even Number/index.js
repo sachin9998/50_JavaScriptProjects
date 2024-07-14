8b6289713ca372dbd0e59c089e2cf094434a3efe
Sachin Alam <sachinalam4216@gmail.com>
2024-07-14 14:31:07 +0530

    Task - 6 Added

--- 6 - Odd or Even Number/index.js
+++ 6 - Odd or Even Number/index.js
@@ -1,1 +1,15 @@
+// Write a function that checks if a number is even or odd and returns "Even" or "Odd" accordingly.
 
+const num = 10;
+const num2 = 3;
+
+function checkNum(num) {
+	if (num % 2 === 0) {
+		return "Even";
+	}
+
+	return "Odd";
+}
+
+console.log(checkNum(num));
+console.log(checkNum(num2));
