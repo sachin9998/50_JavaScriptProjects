c34faf7f61bb6913bbda4bc1106bfc70dfc1afc2
Sachin Alam <sachinalam4216@gmail.com>
2024-07-23 11:44:08 +0530

    24

--- 24 - Private Variable using Closure/index.js
+++ 24 - Private Variable using Closure/index.js
@@ -1,1 +1,27 @@
+// Implement a private variable using closures
 
+function createCounter() {
+  // Private variable
+
+  let count = 0;
+
+  function increment() {
+    count++;
+    console.log("Count:: ", count);
+  }
+
+  return {
+    incrementCounter: function () {
+      increment();
+    },
+    getCount: function () {
+      return count;
+    },
+  };
+}
+
+const counter = createCounter();
+// console.log(counter.getCount());
+console.log(counter.incrementCounter());
+console.log(counter.getCount());
+console.log(counter.incrementCounter());
